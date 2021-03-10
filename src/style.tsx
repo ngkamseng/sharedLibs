80ef7798a9268c3c6eaf4b693dd2d564b7ee4a97
Ng Kam Seng <mike@example.com>
2021-03-10 10:10:52 +0800

    test

---
+++ src/style.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titles: {
+    fontSize:30,
+    margin:20,
+  },
+  content: {
+    fontSize:20,
+    margin:10,
+  }
+});
